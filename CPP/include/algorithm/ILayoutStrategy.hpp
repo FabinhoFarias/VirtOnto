2f895686f6cb86984c7ee7c9133e4a02d53b6e42
Uirajan <ujs@cin.ufpe.br>
2026-09-25 20:33:55 -0300

    review: analise da estrutura concluída

--- CPP/include/algorithm/ILayoutStrategy.hpp
+++ CPP/include/algorithm/ILayoutStrategy.hpp
@@ -6,8 +6,10 @@ namespace virtonto::algorithm {
 
 class ILayoutStrategy {
 public:
-    virtual ~ILayoutStrategy() = 0;
+    virtual ~ILayoutStrategy() = default;
     virtual void applyLayout(model::Graph& graph, int iterations) = 0;
 };
 
 } // namespace virtonto::algorithm
+
+//concluído
